80bca52d4bb2852594eb8887a89d069079728439
Antony Rafael <depaulacostaantony@gmail.com>
2021-09-01 18:42:42 -0300

    search and serialize url

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,10 +1,29 @@
 <template>
-  <div></div>
+  <form>
+    <input type="text" name="busca" id="busca" v-model="busca" />
+    <input
+      type="submit"
+      name="buscar"
+      id="buscar"
+      value="Buscar"
+      @click.prevent="buscarProdutos"
+    />
+  </form>
 </template>
 
 <script>
 export default {
   name: "ProdutosBuscar",
+  data() {
+    return {
+      busca: "",
+    };
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } });
+    },
+  },
 };
 </script>
 
--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -2,32 +2,44 @@
   <section class="produtos-container">
     <div v-for="produto in produtos" :key="produto.id">
       <img
-        v-if="produtos.fotos"
-        :src="produto.fotos[0]"
+        v-if="produto.fotos"
+        :src="produto.fotos[0].src"
         :alt="produto.fotos[0].titulo"
       />
       <p class="preco">{{ produto.preco }}</p>
-      <h2 class="titulo">
-        {{ produto.nome }}
-      </h2>
-      <p class="descricao">{{ produto.descricao }}</p>
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p>{{ produto.descricao }}</p>
     </div>
   </section>
 </template>
 
 <script>
 import { api } from "@/services.js";
+import { serialize } from "@/helpers.js";
 
 export default {
-  name: "ProdutosLista",
   data() {
     return {
       produtos: null,
+      produtosPorPagina: 9,
     };
+  },
+  computed: {
+    url() {
+      const query = serialize(this.$route.query);
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
+    },
   },
   methods: {
     getProdutos() {
-      api.get("/produto").then(({ data }) => (this.produtos = data));
+      api.get(this.url).then((response) => {
+        this.produtos = response.data;
+      });
+    },
+  },
+  watch: {
+    url() {
+      this.getProdutos();
     },
   },
   created() {
